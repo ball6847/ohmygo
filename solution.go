6836c0512db6185a081c07dccfee79f3de829d94
Porawit Poboonma <ball6847@gmail.com>
2018-06-03 13:01:49 +0700

    add interface solution

--- solution.go
+++ solution.go
@@ -5,6 +5,24 @@ type mammal interface {
 	canFeed() bool
 }
 
+type man struct{}
+
+func (man) canWalk() bool {
+	return true
+}
+func (man) canFeed() bool {
+	return true
+}
+
+type dog struct{}
+
+func (dog) canWalk() bool {
+	return true
+}
+func (dog) canFeed() bool {
+	return true
+}
+
 func newMan() mammal {
 	return man{}
 }
